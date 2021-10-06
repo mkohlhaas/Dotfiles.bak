0e6e416d0cc3973de062c5a585d1b46a3712e08f
Hans-J. Schmid <knock@myopendoor.de>
2021-10-07 00:28:08 +0200

    adds buffer navigation shortcuts to nvim

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -4,3 +4,6 @@ utils.map('n', '<Leader>ff', '<cmd>Telescope find_files<cr>')
 utils.map('n', '<Leader>fg', '<cmd>Telescope live_grep<cr>')
 utils.map('n', '<Leader>fb', '<cmd>Telescope buffers<cr>')
 utils.map('n', '<Leader>fh', '<cmd>Telescope help_tags<cr>')
+-- Move to next and previous buffers.
+utils.map('n', '<M-Right>', ':bn<CR>')
+utils.map('n', '<M-Left>',  ':bn<CR>')
