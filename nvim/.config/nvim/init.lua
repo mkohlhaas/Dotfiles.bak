7f47e39241c81cbd8647255d149f1ae10d6beec1
Hans-J. Schmid <knock@myopendoor.de>
2022-01-04 10:45:32 +0100

    cleans up nvim config.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,30 +8,14 @@ local execute = vim.api.nvim_command
 -- Auto install packer.nvim if not exists
 local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    execute('!git clone https://github.com/wbthomason/packer.nvim ' ..
-                install_path)
+  execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
+
 vim.cmd [[packadd packer.nvim]]
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
+vim.cmd [[match errorMsg /\s\+$/]]                       -- show trailing whitespace
 
-vim.cmd [[match errorMsg /\s\+$/]] -- show trailing whitespace
-
--- Sensible defaults
 require('settings')
--- Install plugins
 require('plugins')
--- Key mappings
 require('keymappings')
--- LSP
 require('lsp')
-
--- Another option is to groups configuration in one folder
--- require('config')
-
--- DAP
--- require('dbg')
-
--- OR you can invoke them individually here
--- require('config.colorscheme')  -- color scheme
--- require('config.completion')   -- completion
--- require('config.fugitive')     -- fugitive
