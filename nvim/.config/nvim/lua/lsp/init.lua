a57e4a1dab3c93e18817f90ec3ac38ab41bd71ae
Hans-J. Schmid <knock@myopendoor.de>
2022-05-16 11:59:34 +0200

    changes nvim format command.

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -30,7 +30,7 @@ local on_attach = function(_, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap("n", "<space>fo", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
 
 -- Register a handler that will be called for all installed servers.
