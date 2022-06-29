39c36235911cf65589479f08bc9872c45d343f72
Hans-J. Schmid <knock@myopendoor.de>
2022-06-29 04:26:27 +0200

    adds super- and subscripts.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,12 +4,9 @@ local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   PackerBootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
--- vim.cmd 'packadd packer.nvim'
--- vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
 require('plugins')
 require('telescope').load_extension('fzf')
--- require('telescope').load_extension('hoogle')
 require('telescope').setup{ defaults = { file_ignore_patterns = { "node_modules" } } }
 require('settings')
 require('keymappings')
