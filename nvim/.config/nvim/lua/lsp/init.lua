e7c1ba3b0fd6fff1ae937c745ed2f1b879a0163e
Hans-J. Schmid <knock@myopendoor.de>
2022-11-12 22:28:47 +0100

    changes nvim format command.

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -30,7 +30,7 @@ local on_attach = function(_, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<space>qq', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap("n", "<space>fo", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap("n", "<space>fo", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
   buf_set_keymap("n", "<leader>cl","<cmd>lua vim.lsp.codelens.refresh()<CR>", opts)
   buf_set_keymap("n", "<leader>cc", "<cmd>lua vim.lsp.codelens.run()<CR>", opts)
 end
